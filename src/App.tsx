eda157316cd2d0312f3f76e0857f1080307c5b7a
Lucas Campanelli <lucas.campanelli@outlook.com>
2024-06-25 22:36:06 -0300

    Criação do Service de filmes e implementação da obtenção de filmes populares da API TMDB

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Header } from './components/Header'
 import { MoviesList } from './components/MoviesList'
 import { MoviesConteiner } from './styles/MoviesConteiner'
+import { MovieService } from './api/MovieService'
 
 
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+
+    // Obtendo o atributo "data" da resposta da API
+    const { data}  = await MovieService.getMovies();
+
+    setMovies(data.results)
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
 
   return (
     <>
       <Header/>
 
       <MoviesConteiner>
-        <MoviesList movies={[]}/>
+        <MoviesList movies={movies}/>
       </MoviesConteiner>
     </>
   )
---
+++ src/api/MovieService.tsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { API_KEY } from "./ApiAuth";
+
+const BASE_URL = 'https://api.themoviedb.org/3/';
+
+const withBaseUrl = (path: string) => `${BASE_URL}${path}?api_key=${API_KEY}`;
+
+export class MovieService {
+    static getMovies() {
+        return axios(withBaseUrl('/movie/popular'));
+    }
+}
